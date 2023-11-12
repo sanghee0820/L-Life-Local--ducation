f97d2ac945a350a0e03b8dae533f5244cd6a258c
Sanghee0820 <lsh277604@gmail.com>
2023-11-12 15:24:05 +0900

    Feat: add Paging

--- src/main/java/Lducation/demo/course/service/CourseService.java
+++ src/main/java/Lducation/demo/course/service/CourseService.java
@@ -2,6 +2,7 @@ package Lducation.demo.course.service;
 
 import Lducation.demo.course.domain.Course;
 import Lducation.demo.course.dto.CourseDTO;
+import Lducation.demo.course.dto.PageDTO;
 import Lducation.demo.course.repository.CourseRepository;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,14 +16,39 @@ import org.springframework.stereotype.Service;
 public class CourseService {
     private final CourseRepository courseRepository;
 
-    public List<CourseDTO> getCourseInfoByGu(String gu) {
+    private boolean validPageSize(Long page, Long maxPage) {
+        if (page < 0 || page > maxPage) {
+            return true;
+        }
+        return false;
+    }
 
-        List<Course> courses = courseRepository.findByGu(gu);
-        List<CourseDTO> courseDTOs = courses.stream()
+    private List<CourseDTO> splitCourses(List<Course> courses, int startIndex, int endIndex) {
+        return courses.subList(startIndex, endIndex)
+                .stream()
                 .map(CourseDTO::toDTO)
                 .collect(Collectors.toList());
-        log.info(String.valueOf(courses.isEmpty()));
-        return courseDTOs;
+    }
+
+
+    public PageDTO getCourseInfoByGu(String gu, Long page) {
+        List<Course> courses = courseRepository.findByGu(gu);
+        log.info(String.valueOf(courses.size()));
+        int pageSize = 9;
+        if (validPageSize(page, (long) (courses.size() / 9))) {
+            return null;
+        }
+        int startIndex = Math.toIntExact(page * pageSize);
+        int endIndex = Math.min(Math.toIntExact((page + 1) * pageSize), courses.size());
+
+        List<CourseDTO> courseDTOs = splitCourses(courses, startIndex, endIndex);
+
+        PageDTO pageDTO = PageDTO.builder()
+                .page(page)
+                .maxPage(Long.valueOf(courses.size() / pageSize))
+                .courses(courseDTOs)
+                .build();
+        return pageDTO;
     }
 
     public List<CourseDTO> getCourseInfoAll() {
