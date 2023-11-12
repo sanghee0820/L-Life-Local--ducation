c11fbd967e926b9f1edaf7e7433485a40bc180db
Sanghee0820 <lsh277604@gmail.com>
2023-11-12 21:53:39 +0900

    Refactor: merge pageInfo

--- src/main/java/Lducation/demo/course/controller/InfoController.java
+++ src/main/java/Lducation/demo/course/controller/InfoController.java
@@ -33,8 +33,8 @@ public class InfoController {
     public ResponseEntity<PageDTO> getCourse(
             @RequestParam(value = "gu", required = true) final String gu,
             @RequestParam(value = "page", required = true) final Long page) {
-        PageDTO pageInfo;
-        pageInfo = courseService.getCourseInfoByGu(gu, page);
+        
+        PageDTO pageInfo = courseService.getCourseInfoByGu(gu, page);
         if (pageInfo == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
